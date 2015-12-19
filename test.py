724c4fd9e2c4b1b7d08441e30e3690eba4d17d7f
Alan Villa <a19i23villa@gmail.com>
2015-12-18 20:33:57 -0600

    adding to file

---
+++ test.py
@@ -0,0 +1,13 @@
+#
+
+import urllib2
+import json
+
+locu_api = '6252bab312fd63a8b43f273bbbc5b8ae973d9822'
+
+url = 'https://api.locu.com/v1_0/venue/search/?has_menu=TRUE&api_key=6252bab312fd63a8b43f273bbbc5b8ae973d9822'
+json_obj = urllib2.urlopen(url)
+
+data = json.load(json_obj)
+
+print data
