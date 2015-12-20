2e2ef6d4169d0f9f86e3b1c2198b62002d6b2cd2
Alan Villa <a19i23villa@gmail.com>
2015-12-20 16:47:54 -0600

    adding to file

---
+++ todoist.py
@@ -0,0 +1,22 @@
+import todoist
+api = todoist.TodoistAPI('24741046aac920c0d5b7174e62bb6c6c0c4cbe3d')
+api.sync(resource_types=['all'])
+{
+  'CollaboratorStates': [],
+  'Collaborators': [],
+  'DayOrders': {},
+  'DayOrdersTimestamp': '1344642991.1',
+  'LiveNotificationsLastRead': 0,
+  'UserId': 1855589,
+  'WebStaticVersion': 305,
+  'seq_no': 2180537512L,
+  'Filters': [],
+  'Items': [],
+  'Labels': [],
+  'LiveNotifications': [],
+  'Notes': [],
+  'Projects': [],
+  'Reminders': [],
+  'User': {
+  }
+}
